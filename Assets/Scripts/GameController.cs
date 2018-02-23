5fc709623d0c1005830ae88763ade90cf80bcf2a
Michael Loubier <michael.a.loubier@gmail.com>
2018-02-22 22:08:24 -0800

    TapTarget asset v1 created.
    TapTarget v1 can detect onTap for touchscreen and onClick for Editor testing. Debug logs have been disabled, though the code is only commented out at this point.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject TapTargetPrefab;
+
+	float lastSpawnTime = 0.0f;
+
+	float nextSpawnDelay;
+
+	Vector3 nextSpawnPosition;
+
+	// Use this for initialization
+	void Start () {
+		nextSpawnDelay = Random.Range (1f, 5f);
+		nextSpawnPosition = new Vector3 (Random.Range (-6f, 7f), Random.Range (-2.5f, 4.25f), 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= lastSpawnTime + nextSpawnDelay) {
+			lastSpawnTime = Time.time;
+
+			TapTarget nextTarget = Instantiate (TapTargetPrefab, nextSpawnPosition, Quaternion.identity).GetComponent<TapTarget>();
+			nextTarget.decaySpeed = Random.Range (0.2f, 2f);
+			nextSpawnPosition = new Vector3 (Random.Range (-6f, 7f), Random.Range (-2.5f, 4f), 1f);
+		}
+	}
+}
---
+++ Assets/Scripts/TapTarget.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTarget : MonoBehaviour {
+
+	// How fast will this target decay?
+	public float decaySpeed = 5.0f;
+
+	// How big is the outer ring (TimerRing) currently, compared to the size of the Target Ring
+	float timerRingScale = 3.0f;
+
+	// Reference for the TimerRing
+	public GameObject TimerRing;
+
+	// Reference for the TargetRing
+	public GameObject TargetRing;
+
+	// Interpolation parameter tells the percentage of the transformation that has taken place in the Lerp function
+	float tParam = 0f;
+
+	// We want to delay slightly after the rings align to destroy the ring to give users a bit of a chance to get the 100% click
+	bool aligned = false;
+	float alignTime = 0.0f;
+	float delayTime;
+
+	void Update()
+	{
+//		print (tParam);
+		// Scale the TimerRing down over time
+		if (tParam < 1) {
+			tParam += Time.deltaTime * decaySpeed; //This will increment tParam based on Time.deltaTime multiplied by a speed multiplier
+			timerRingScale = Mathf.Lerp (3f, 1f, tParam);
+			TimerRing.transform.localScale = new Vector3 (timerRingScale, timerRingScale, 1f);
+		} else if (aligned == false) {
+//			Debug.Log( "Last chance!");
+			alignTime = Time.time;
+//			Debug.Log (alignTime);
+			aligned = true;
+		} else if (aligned && Time.time >= alignTime + delayTime) {
+//			Debug.Log ("Too slow!");
+			Destroy (this.gameObject);
+		}
+
+		#if UNITY_EDITOR && !UNITY_IOS
+		if (Input.GetButtonDown("Fire1")) {
+//			Debug.Log ("Touch detected.");
+			RaycastHit2D raycastHit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			if (raycastHit) {
+				if (raycastHit.collider.CompareTag("TapTarget")) {
+					Debug.Log("TapTarget clicked");
+				}
+			}
+		}
+
+		#elif UNITY_IOS
+		if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) {
+//			Debug.Log ("Touch detected.");
+			RaycastHit2D raycastHit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
+			if (raycastHit) {
+				if (raycastHit.collider.CompareTag("TapTarget")) {
+//					Debug.Log("TapTarget clicked");
+				}
+			}
+		}
+
+		#endif
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		delayTime = 0.1f / decaySpeed;
+//		print ("DelayTime = " + delayTime);
+	}
+}
