59623549c71ce8a9b47b3acfb464ac0d11c182c4
Michael Loubier <hassurunous@gmail.com>
2018-05-21 15:53:00 -0700

    Importing AudioVisualizer for future update. Created some files for modularizing TapTarget functionality a bit more.

---
+++ Assets/Scripts/BeatSpawnController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSpawnController : MonoBehaviour {
+
+    // Store for spawning TapTargets
+    [SerializeField]
+    GameObject TapTargetPrefab;
+
+    // Store for Accuracy Indicator prefab
+    [SerializeField]
+    GameObject AccuracyIndicatorPrefab;
+
+    void OnEnable()
+    {
+        TapTargetEventManager.OnTapped += OnTapTargetTapped;
+    }
+
+
+    void OnDisable()
+    {
+        TapTargetEventManager.OnTapped -= OnTapTargetTapped;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTapTargetTapped(Vector3 position,  float accuracy)
+    {
+        print("Target tapped. Accuracy = " + (int)(accuracy * 100));
+    }
+}
